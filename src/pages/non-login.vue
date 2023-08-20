f355b2c488807162d16fd128438bfc36d5ea7122
Stan <lol97554@gmail.com>
2023-08-21 00:10:32 +0800

    feat: go to login page with authenticate service.

--- src/pages/non-login.vue
+++ src/pages/non-login.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+const goLogin = () => {
+  window.location.href = (import.meta.env.DEV) ? 'http://localhost:3000/auth/login' : 'https://aha-api.stanma.dev/auth/login'
+}
 </script>
 
 <template>
@@ -8,7 +11,7 @@
         Get Started
       </h1>
       <div class="flex flex-row space-x-4 py-2">
-        <button class="btn btn-primary">
+        <button class="btn btn-primary" @click="goLogin">
           Login
         </button>
         <button class="btn">
