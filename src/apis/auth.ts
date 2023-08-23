f8f438d1a1bea1ae29701c33537c97f076a4afe2
Stan <lol97554@gmail.com>
2023-08-23 23:20:12 +0800

    feat: implement getProfile & updateName apis.

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -21,6 +21,24 @@ const checkLoggedIn = async () => {
   return false;
 };
 
+const getProfile = async () => {
+  const res = await api.get("/member")
+  if (res.status == 200) {
+    return res.data;
+  }
+  return false;
+}
+
+const updateName = async (name: string) => {
+  const res = await api.put("/member", { name })
+  if (res.status == 200) {
+    return res.data;
+  }
+  return false;
+};
+
 export default {
   checkLoggedIn,
+  getProfile,
+  updateName,
 }
